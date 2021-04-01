aa3ac00f1893f6d597e33c23883dfe8ca5baa101
MalavikaAjit <malavikaajit9@gmail.com>
2021-04-02 00:18:18 +0530

    ADD: added User register program

---
+++ src/com/company/UserRegistrationMain.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistrationMain {
+    //validating user input first name
+    public static void validateFirstName() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String firstName =scan.next();
+        String pattern = "^[A-Z]{1}[a-z]{3}$";
+        System.out.println("first name is " +firstName);
+        System.out.println("Is the First name valid? " + firstName.matches(pattern));
+
+    }
+    public static void main(String[] args) {
+	validateFirstName();
+	}
+}
