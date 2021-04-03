29168d9d81c4dec26281e6dc27b865d39bb3ad27
Malavika Ajit <78484044+MalavikaAjit@users.noreply.github.com>
2021-04-03 06:49:59 +0530

    MODIFIED: edited lines

--- src/com/company/UserRegistrationMain.java
+++ src/com/company/UserRegistrationMain.java
@@ -9,7 +9,7 @@ public class UserRegistrationMain {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter first name");
         String firstName =scan.next();
-        String pattern = "^[A-Z]{1}[a-z]{3}$";
+        String pattern = "^[A-Z]{1}[a-z]{2,}$";
         System.out.println("first name is " +firstName);
         System.out.println("Is the First name valid? " + firstName.matches(pattern));
 
@@ -18,7 +18,7 @@ public class UserRegistrationMain {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Last name");
         String lastName =scan.next();
-        String pattern = "^[A-Z]{1}[a-z]{3}$";
+        String pattern = "^[A-Z]{1}[a-z]{2,}$";
         System.out.println("last name is " +lastName);
         System.out.println("Is the Last name valid? " + lastName.matches(pattern));
 
