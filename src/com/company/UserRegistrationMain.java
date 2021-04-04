32e86bd18f7da865d406c0f7124588a161d8aee8
MalavikaAjit <malavikaajit9@gmail.com>
2021-04-04 19:11:57 +0530

    ADD: added feature to clear all email samples

--- src/com/company/UserRegistrationMain.java
+++ src/com/company/UserRegistrationMain.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -23,13 +24,13 @@ public class UserRegistrationMain {
         System.out.println("Is the Last name valid? " + lastName.matches(pattern));
 
     }
-    public static void validateEmail() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter email");
-        String email =scan.next();
+    public static void validateEmail(ArrayList<String> emails) {
         String pattern = "^[a-zA-Z0-9]+([.+_-]{0,1}+[a-zA-Z0-9])*+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.]{1}+[a-zA-Z]{0,2})*";
-        System.out.println("last name is " +email);
-        System.out.println("Is the email valid? " + email.matches(pattern));
+       System.out.println("email is " +emails);
+
+        for ( String email:emails) {
+            System.out.println("Is the email valid? " + email.matches(pattern));
+        }
 
     }
     public static void validatePhoneno() {
@@ -45,7 +46,7 @@ public class UserRegistrationMain {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter password");
         String password =scan.next();
-        String pattern = "^[a-zA-Z0-9]{8}$";
+        String pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
         System.out.println("password is " +password);
         System.out.println("Is the password valid? " + password.matches(pattern));
 
@@ -53,8 +54,30 @@ public class UserRegistrationMain {
     public static void main(String[] args) {
 	validateFirstName();
 	validateLastName();
-	validateEmail();
 	validatePhoneno();
 	validatePassword();
-	}
+
+	ArrayList<String> emails = new ArrayList<String>();
+    emails.add("abc@yahoo.com");
+    emails.add("abc-100@yahoo.com");
+    emails.add("abc.100@yahoo.com");
+    emails.add("abc111@abc.com");
+    emails.add("abc-100@abc.net");
+    emails.add("abc.100@abc.com.au");
+    emails.add("abc@1.com");
+    emails.add("abc@gmail.com.com");
+    emails.add("abc");
+    emails.add("abc@.com.my");
+    emails.add("abc123@gmail.a");
+    emails.add("abc123@.com");
+    emails.add("abc123@.com.com");
+    emails.add(".abc@abc.com");
+    emails.add("abc()*@gmail.com");
+    emails.add("abc..2002@gmail.com");
+    emails.add("abc.@gmail.com");
+    emails.add("abc@gmail.com.1a");
+    emails.add("abc@gmail.com.aa.au");
+    validateEmail(emails);
+    }
+
 }
